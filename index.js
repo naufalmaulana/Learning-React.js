38ad8c732e70fd886c5fa3be717e8cc09677ea39
naufalmaulana <naufallmaulana@yahoo.com>
2022-02-08 16:29:15 +0700

    Lesson 2

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-ReactDOM.render(<h1>Hello, everyone! </h1>, document.getElementById("root"))
+ReactDOM.render(
+<ul>
+    <li>List 1</li>
+    <li>List 2</li>
+</ul>, document.getElementById("root"))
