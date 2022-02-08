3f0e5a0d17bf7fb952e1bb35a29fb430b56b8b40
naufalmaulana <naufallmaulana@yahoo.com>
2022-02-09 01:02:38 +0700

    Lesson 8

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
-function Header(){
-    return (        
-        <nav>
-            <img src="./react-logo.png" width="40px" />
-        </nav>
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="./react-logo.png" width="40px" />
+            </nav>
+        </header>
     )
 }
-function Page() {
+
+function Footer() {
+    return (
+        <footer>
+            <small>© 2021 Ziroll development. All rights reserved.</small>
+        </footer>
+    )
+}
+
+function MainContent() {
     return (
         <div>
-        <h1>Reasons i'm excited to learn React</h1>
-        <ol>
-            <li>It's a popular library, so I'll be 
-            able to fit in with the cool kids!</li>
-            <li>I'm more likely to get a job as a developer
-            if I know React</li>
-        </ol>      
-        </div>
-    )
-}
-function Footer(){
-    return (        
-        <div>
-            <p>© 20xx name development. All rights reserved.</p>
+            <h1>Reasons I'm excited to learn React</h1>
+            <ol>
+                <li>It's a popular library, so I'll be 
+                able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                if I know React</li>
+            </ol>
         </div>
     )
 }
 
-ReactDOM.render(
-    <div>
-        <Header />
-        <Page />
-        <Footer />
-    </div>
-    , document.getElementById("root"))
+function Page() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    )
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
