582b305d4eb17d970a86399652a094e2569d2b14
naufalmaulana <naufallmaulana@yahoo.com>
2022-02-08 22:11:28 +0700

    Lesson 5

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
-// JSX
+// these are used if you are creating react project without cdns
+import React from "react"
+import ReactDOM from "react-dom"
+
+/**
+Challenge: fix our code!
+
+Don't forget, you're not using CDNs anymore, so there's no
+global "ReactDOM" variable to use anymore.
+ */
+
 const page = (
     <div>
-        <h1 className="header">This is JSX</h1>
-        <p>This is a paragraph</p>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
     </div>
 )
+
 ReactDOM.render(
     page,
     document.getElementById("root")
